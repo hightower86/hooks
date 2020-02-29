d7bfb955fbddbccd0b2ff062c3ada4e5a30e6dbc
Adel <htwr86@gmail.com>
2020-02-29 17:46:22 +0300

    96 UseState()

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useState } from 'react';
 import ReactDom from 'react-dom';
 
 //const MyContext = React.createContext();
@@ -6,11 +6,51 @@ import ReactDom from 'react-dom';
 const App = () => {
   return (
     <div>
-      <h1>Hi</h1>
+      <Hook />
     </div>
     // <MyContext.Provider value='Hello world'>
     //   <Child />
     // </MyContext.Provider>
+  );
+};
+
+const Hook = () => {
+  const [color, setColor] = useState('white');
+  const [fontSize, setFontSize] = useState(20);
+  return (
+    <div
+      style={{
+        backgroundColor: color,
+        padding: '10px',
+        fontSize: `${fontSize}px`
+      }}
+    >
+      Hello World
+      <button
+        style={{ padding: '10px', margin: '5px' }}
+        onClick={() => setColor('white')}
+      >
+        white
+      </button>
+      <button
+        style={{ padding: '10px', margin: '5px' }}
+        onClick={() => setColor('#999')}
+      >
+        Gray
+      </button>
+      <button
+        style={{ padding: '10px', margin: '5px' }}
+        onClick={fontSize => setFontSize(s => s + 2)}
+      >
+        +
+      </button>
+      <button
+        style={{ padding: '10px', margin: '5px' }}
+        onClick={fontSize => setFontSize(s => s - 2)}
+      >
+        -
+      </button>
+    </div>
   );
 };
 
